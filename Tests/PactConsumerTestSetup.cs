48adb34b45026e2f8a2b05fe46d118a1603d3510
Richard Peckett <richard.peckett@outlook.com>
2018-06-08 15:25:34 +0100

    first pass at wrapper code for provider tests

--- Tests/PactConsumerTestSetup.cs
+++ Tests/PactConsumerTestSetup.cs
@@ -122,53 +122,11 @@ namespace Tests
             return this;
         }
 
-        public PactMessageSetup MockCheckResponse()
+        public PactMessageSetup MockCheckResponse(string path)
         {
-            Client.Get("/servicebusmock/message", HttpStatusCode.OK);
+            Client.Get(path, HttpStatusCode.OK);
 
             return this;
-        }
-
-        public IMockProviderService Foo()
-        {
-            // in a real project the tests which set the expectation of the consumer would
-            // probably live in a project away from the actual service itself
-            // Here we set the expectation of the message format (contract) the PLR service
-            // expects to receive from the achievement service
-
-            MockMessageService.Given("There is a new a message in the achievements queue")
-                .UponReceiving("A request to process the message")
-                .With(new ProviderServiceRequest
-                {
-                    // Define what the message should contain
-                    Method = HttpVerb.Get, // hide in base class
-                    Path = "/servicebusmock/message",
-                    Headers = new Dictionary<string, object>
-                    {
-                        {"Accept", "application/json"}
-                    },
-                    Query = ""
-                })
-                .WillRespondWith(new ProviderServiceResponse
-                {
-                    Status = 200,
-                    Headers = new Dictionary<string, object>()
-                    {
-                        {"Content-Type", "application/json; charset=utf-8"}
-                    },
-                    Body =
-                        new
-                        {
-                            CertificateId = "ae553ab2-51f1-40f0-81eb-95e46205d6e5",
-                            LearnerId = "3bb86580-aef6-4526-81e7-35bb22a4390e",
-                            CorrelationId = "2b07dd51-1a63-4834-9311-36667ec89f51",
-                            Type = "Achievement",
-                        }
-                });
-
-            Client.Get("/servicebusmock/message", HttpStatusCode.OK);
-
-            return MockMessageService;
         }
 
         public PactMessageSetup MockVerifyAndClearInteractions()
